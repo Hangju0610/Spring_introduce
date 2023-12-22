9bc3efb0b2489bc61fd7240e78a11bd32efe577c
hangju0610 <phj950610@naver.com>
2023-12-22 14:23:23 +0900

    Feat: Add JpaRepository JPA를 통한 Repository 계층 작성 JDBC Template보다 더욱 편하계 작성할 수 있다.

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,10 +1,9 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,11 +12,17 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;
+    private final EntityManager entityManager;
 
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    @Autowired
+    public SpringConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
     }
+//    private final DataSource dataSource;
+
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
 
     @Bean
     public MemberService memberService() {
@@ -28,6 +33,7 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JapMemberRepository(entityManager);
     }
 }
--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,10 +1,17 @@
 package hello.hellospring.domain;
 
+import jakarta.persistence.*;
+
 /**
  * 회원 Domain Class
  */
+@Entity
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB에서 직접 생성하기 때문에 IDENTITY라고 한다.
     private Long id;
+
+//    @Column(name= "username") // name이 DB에서 username일 경우 mapping 해준다.
     private String name;
 
     public Long getId() {
